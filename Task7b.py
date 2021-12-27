5ad9e0ed6dafaf02a3de90734b5533310ff56e4c
Rick <ricksmycodeguy@gmail.com>
2021-12-27 16:50:56 -0600

    final push of the day

--- Task7b.py
+++ Task7b.py
@@ -1,10 +1,10 @@
 def removeDupes(s):
-    arrayOfWords = s.split() ## creates an array of words
+    arrayOfWords = s.split() ## creates an array of words from the given string "s".
     noDupes = []
     for eachWord in arrayOfWords:
         if eachWord not in noDupes:
             noDupes.append(eachWord)
-    return " ".join(noDupes)
+    return " ".join(noDupes) ## returns converted array into a string with spaces inbetween each word
     
 print(removeDupes('hi hi hello'))
 print(removeDupes("Hi I am rick rick !"))
